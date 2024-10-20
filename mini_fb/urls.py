65e405bfd56b36f97b11a755a419d5b77d92d8f8
JordanEspinal17 <jmile@bu.edu>
2024-10-20 12:56:03 -0400

    task 2 done

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -4,6 +4,6 @@ from .views import ShowAllProfilesView, ShowProfilePageView, CreateProfileView, 
 urlpatterns = [
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),  # All profiles
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),  # Single profile
-    path('profile/<int:pk>/create_status/', CreateStatusMessageView.as_view(), name='create_status'),  # Status message creation
+    path('profile/<int:pk>/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),  # Profile creation
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,8 +1,10 @@
-from django.views.generic import ListView, DetailView
-from .models import Profile, StatusMessage
+from django.views.generic import ListView, DetailView,CreateView
+from .models import Profile, StatusMessage, Image
 from .forms import CreateProfileForm, CreateStatusMessageForm
 from django.urls import reverse
 from django.views.generic.edit import CreateView
+from django.shortcuts import get_object_or_404, redirect
+
 
 class ShowAllProfilesView(ListView):
     model = Profile  # This fetches all profiles from the database
@@ -19,24 +21,38 @@ class CreateProfileView(CreateView):
     template_name = 'mini_fb/create_profile_form.html'
 
     def get_success_url(self):
-        return reverse('show_profile', kwargs={'pk': self.object.pk})  # After creating, redirect to the profile page
+    # Redirect back to the profile page using the profile's pk from the URL kwargs
+        return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
+
     
 class CreateStatusMessageView(CreateView):
     model = StatusMessage
-    form_class = CreateStatusMessageForm
+    fields = ['message']
     template_name = 'mini_fb/create_status_form.html'
+
+    def form_valid(self, form):
+        # Associate the status message with the correct profile
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        form.instance.profile = profile
+
+        # Save the form and set self.object
+        response = super().form_valid(form)
+
+        # Handle the uploaded files
+        files = self.request.FILES.getlist('files')
+        for f in files:
+            img = Image(image_file=f, status_message=self.object)
+            img.save()
+
+        return response
+
+    def get_success_url(self):
+        # Use self.object.profile.pk or self.kwargs['pk']
+        return reverse('show_profile', kwargs={'pk': self.object.profile.pk})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])  # Get the Profile based on the pk in the URL
+        # Pass the profile to the template context
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        context['profile'] = profile
         return context
-
-    def form_valid(self, form):
-        # Attach the status message to the profile
-        profile = Profile.objects.get(pk=self.kwargs['pk'])
-        form.instance.profile = profile
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        # After submission, redirect to the profile page
-        return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
